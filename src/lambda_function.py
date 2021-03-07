4e544c314e61ff4da519da07943a42325917b524
Lewis Craik <mail@lewiscraik.co.uk>
2021-03-07 21:10:23 +0000

    Added logic for days until we can ride

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,12 +2,44 @@ import os
 import random
 import json
 import tweepy
+import datetime
 
+# Build the tweet
 def get_tweet():
-    """Get tweet to post from CSV file"""
 
-    tweet_text = "In England we can ride locally, with 1 friend. "
+    # Set the dates
+    today = datetime.date.today()           # Today
+    step1 = datetime.date(2021, 3, 29)      # Travel/5 friends
+    step2 = datetime.date(2021, 5, 17)      # Groups up to 30
+    step3 = datetime.date(2021, 6, 21)      # As many people as you like
 
+    # Logic
+    if today < step1:
+        current = "In England we can ride locally, with 1 friend. "
+        days = abs(step1 - today)
+        if days.days == 1:
+            future = "Tomorrow we will be able to travel within England and ride with 5 friends!"
+        else:
+            future = "In " + str(days.days) + " days we will be able to travel within England and ride with 5 friends!"
+    elif today < step2:
+        current = "In England we can travel within England and ride with 5 friends. "
+        days = abs(step2 - today)
+        if days.days == 1:
+            future = "Tomorrow we will be able to ride in a group of up to 30 people, or travel abroad to ride."
+        else:
+            future = "In " + str(days.days) + " days we will be able to ride in a group of up to 30 people, or travel abroad to ride."
+    elif today < step3:
+        current = "In England we can travel within England and ride in a group of up to 30 people, or travel abroad to ride. "
+        days = abs(step3 - today)
+        if days.days == 1:
+            future = "Tomorrow we will be able to ride with as many people as we want!"
+        else:
+            future = "In " + str(days.days) + " days we will be able to ride with as many people as we want!"
+    else:
+        current = "In England we can travel within England and ride with as many people as we want to. Or we can travel abroad to ride."
+        future = ""
+    
+    tweet_text = current + future
     return tweet_text
 
 
